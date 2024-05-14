e0f695ff2553b7ad6816f612484cc320828df128
Ilya <virus-birus@mail.ru>
2024-05-14 17:42:18 +0300

    5/14/2024

--- core.js/index.js
+++ core.js/index.js
@@ -1,4 +1,26 @@
 const ImgSlade = document.querySelectorAll('.ImgSlade')
 const points = document.querySelectorAll('.points')
-const leftArrow = document.querySelectorAll('.leftArrow')
-const rightArrow = document.querySelectorAll('.rightArrow')
+const leftArrow = document.querySelector('.leftArrow')
+const rightArrow = document.querySelector('.rightArrow')
+
+ImgSlade[0].classList.remove('activeImg')
+points[0].classList.add('colorPoint')
+
+function poi() {
+  for(let a = 0; a < points.length; a++) {
+    points[a].addEventListener('click', () => {
+        for(let k = 0; k < ImgSlade.length; k++) {
+            ImgSlade[k].classList.add('activeImg') 
+            points[k].classList.remove('colorPoint')
+        }
+          
+        let count = a
+        points[count].classList.add('colorPoint')
+        ImgSlade[count].classList.remove('activeImg')
+        
+    })
+   
+  }
+}
+
+poi()   
